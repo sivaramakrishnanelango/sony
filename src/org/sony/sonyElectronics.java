b91a63f995f06657859382dbb46aa72915462211
sivaramakrishnan <sivaram.e98@gmail.com>
2022-08-19 15:03:12 +0530

    new code is added

--- src/org/sony/sonyElectronics.java
+++ src/org/sony/sonyElectronics.java
@@ -2,4 +2,18 @@ package org.sony;
 
 public class sonyElectronics {
 
+	private void sonyEricsson() {
+		System.out.println("sony ericsson is mobile");
+		
+		
+	}
+	private void sonyTv() {
+		System.out.println("sony bravia is curved tv");
+	}
+	
+	public static void main(String[] args) {
+		sonyElectronics s = new sonyElectronics();
+		s.sonyEricsson();
+		s.sonyTv();
+	}
 }
